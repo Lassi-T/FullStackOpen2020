a0e58cc79ea32e98ba8708fafaa7521397af365e
Lassi Timoskainen <timoskainen.lassi@gmail.com>
2020-05-19 20:12:30 +0300

    Added 1.2  exercise

--- Osa1/1.1/src/index.js
+++ Osa1/1.1/src/index.js
@@ -13,10 +13,16 @@ const Header = (props) => {
 const Content = (props) => {
   return (
     <div>
-      <p>
-        {props.part} {props.exercise}
-      </p>
+      <Part part={props.part[0]} exercise={props.exercise[0]}/>
+      <Part part={props.part[1]} exercise={props.exercise[2]}/>
+      <Part part={props.part[2]} exercise={props.exercise[3]}/>
     </div>
+  )
+}
+
+const Part = (props) => {
+  return (
+    <p>{props.part} {props.exercise}</p>
   )
 }
 
@@ -27,6 +33,7 @@ const Total = (props) => {
 }
 
 const App = () => {
+
   const course = 'Half Stack application development'
   const part1 = 'Fundamentals of React'
   const exercises1 = 10
@@ -38,11 +45,7 @@ const App = () => {
   return (
     <div>
       <Header course={course} />
-
-      <Content part={part1} exercise={exercises1}/>
-      <Content part={part2} exercise={exercises2}/>
-      <Content part={part3} exercise={exercises3}/>
-
+      <Content part={[part1, part2, part3]} exercise={[exercises1, exercises2, exercises3]}/>
       <Total total={exercises1 + exercises2 + exercises3}/>
     </div>
   )
