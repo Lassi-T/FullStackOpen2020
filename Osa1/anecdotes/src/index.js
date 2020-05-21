2b229a76d838dfcfba4bc7025d506623e0765939
Lassi Timoskainen <timoskainen.lassi@gmail.com>
2020-05-21 15:48:23 +0300

    Added 1.13 and 1.14

--- Osa1/anecdotes/src/index.js
+++ Osa1/anecdotes/src/index.js
@@ -1,19 +1,43 @@
 import React, { useState } from 'react'
 import ReactDOM from 'react-dom'
 
-const App = (props) => {
+const App = ({ anecdotes = [] }) => {
   const [selected, setSelected] = useState(0)
+  const [votes, setVote] = useState(anecdotes.map(() => 0))
 
   const randomAnecdote = () => {
-    setSelected(Math.floor(Math.random() * 6))
+    let number = selected
+    while (number === selected) {
+      number = Math.floor(Math.random() * 6)
+    }
+    setSelected(number)
+  }
+
+  const vote = () => {
+    setVote(votes.map((item, i) => (i === selected ? item + 1 : item)))
+  }
+
+  const getMostVotes = () => {
+    let mostVotes = 0
+    for (let i = 0; i < anecdotes.length; i++) {
+      if (votes[i] > votes[mostVotes]) {
+        mostVotes = i
+      }
+    }
+    return mostVotes
   }
 
   return (
     <div>
+      <h1>Anecdote of the day</h1>
       <p>
-        {props.anecdotes[selected]} <br></br>
+        {anecdotes[selected]} <br></br>
+        Votes {votes[selected]} <br></br>
+        <button onClick={vote}>Vote</button>
         <button onClick={randomAnecdote}>Next anecdote</button>
       </p>
+      <h2>Anecdote with the most votes</h2>
+      {anecdotes[getMostVotes()]}
     </div>
   )
 }
@@ -24,10 +48,7 @@ const anecdotes = [
   'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
   'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
   'Premature optimization is the root of all evil.',
-  'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.'
+  'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
 ]
 
-ReactDOM.render(
-  <App anecdotes={anecdotes} />,
-  document.getElementById('root')
-)
+ReactDOM.render(<App anecdotes={anecdotes} />, document.getElementById('root'))
