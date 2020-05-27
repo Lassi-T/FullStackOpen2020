c38cf7bbf0e29be527c39eb1676790b588e80af1
Lassi Timoskainen <timoskainen.lassi@gmail.com>
2020-05-27 22:14:00 +0300

    Added mostly working 2.14

--- Osa2/countrydata/src/components/Weather.js
+++ Osa2/countrydata/src/components/Weather.js
@@ -3,6 +3,20 @@ import axios from 'axios'
 
 const apiKey = process.env.REACT_APP_API_KEY
 
+const Render = ({ weather }) => {
+  return (
+    <div>
+      <div>
+        <strong>temperature:</strong> {weather.current.temperature} Celsius
+      </div>
+      <img src={weather.current.weather_icons.map((icon) => icon)} alt='Weather icon' />
+      <div>
+        <strong>wind:</strong> {weather.current.wind_speed} mph {weather.current.wind_dir}
+      </div>
+    </div>
+  )
+}
+
 const Weather = ({ country }) => {
   const [weather, setWeather] = useState([])
 
@@ -10,6 +24,7 @@ const Weather = ({ country }) => {
     axios
       .get(`http://api.weatherstack.com/current?access_key=${apiKey}&query=${country.capital}`)
       .then((response) => {
+        console.log('data got')
         setWeather(response.data)
       })
   }, [country.capital])
@@ -19,7 +34,7 @@ const Weather = ({ country }) => {
   return (
     <div>
       <h2>Weather in {country.capital}</h2>
-      {weather.length > 0 && console.log('Nyt')}
+      {weather.length !== 0 && <Render weather={weather} />}
     </div>
   )
 }
