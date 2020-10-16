517307d32918802e0ed90e1265ac5020d27ec361
Lassi Timoskainen <timoskainen.lassi@gmail.com>
2020-10-16 23:17:07 +0300

    Part 5.2 ready

--- Osa5/bloglist-frontend/src/App.js
+++ Osa5/bloglist-frontend/src/App.js
@@ -17,12 +17,22 @@ const App = () => {
     )  
   }, [])
 
+  useEffect(() => {
+    const loggedUserJSON = window.localStorage.getItem('loggedUser')
+    if (loggedUserJSON) {
+      const user = JSON.parse(loggedUserJSON)
+      setUser(user)
+      blogService.setToken(user.token)
+    }
+  }, [])
+
   const handleLogin = async (event) => {
     event.preventDefault()
     try {
       const user = await loginService.login({
         username, password,
       })
+      window.localStorage.setItem('loggedUser', JSON.stringify(user))
       setUser(user)
       setUsername('')
       setPassword('')
@@ -32,6 +42,11 @@ const App = () => {
         setErrorMessage(null)
       }, 5000)
     }
+  }
+
+  const handleLogout = () => {
+    window.localStorage.removeItem('loggedUser')
+    window.location.reload()
   }
 
   const loginForm = () => (
@@ -64,7 +79,10 @@ const App = () => {
     <div>
       <h2>Blogs</h2>
       <Notification message={errorMessage} />
-      <p>{user.name} is logged in</p>
+      <p>
+        {user.name} is logged in
+        <button onClick={handleLogout}>logout</button>
+      </p>
       {blogs.map(blog =>
         <Blog key={blog.id} blog={blog} username={user.username}/>
       )}
